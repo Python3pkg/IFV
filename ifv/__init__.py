952e3570cc7a90863b3fafb2047ed53b000e468e
MrLYC <imyikong@gmail.com>
2017-01-05 22:52:10 +0800

    init

---
+++ ifv/__init__.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class BaseAPIItem(object):
+    BASE_CONTEXT = {}
+    IGNORE_ERROR_TYPE = set([KeyboardInterrupt])
+
+    def __init__(self, parent_item=None, **kwargs):
+        self._context = self.BASE_CONTEXT.copy()
+        self._context.update(kwargs)
+        self._parent_item = parent_item
+
+    def _copy(self):
+        return BaseAPIItem(self._parent_item, **self._context)
+
+    def _is_ignore_request_error(self, error):
+        if type(error) in self.IGNORE_ERROR_TYPE:
+            return True
+        return False
+
+    def _handle_request_error(self, error):
+        logger.info(error)
+
+    def _get_response_result(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def _get_subitem_context(self, name):
+        return None
+
+    @property
+    def _merged_context(self):
+        context = {}
+        if self._parent_item:
+            context.update(self._parent_item._merged_context)
+        context.update(self._context)
+        return context
+
+    def __getattr__(self, name):
+        context = self._get_subitem_context(name) or {}
+        return BaseAPIItem(self, **context)
+
+    def __getitem__(self, key):
+        try:
+            return self._context[key]
+        except (IndexError, KeyError):
+            if self._parent_item:
+                return self._parent_item[key]
+            raise
+
+    def __setitem__(self, key, value):
+        self._context[key] = value
+
+    def __iter__(self):
+        return iter(self._merged_context)
+
+    def __len__(self):
+        return len(self._merged_context)
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self._get_response_result(*args, **kwargs)
+        except Exception as error:
+            if self._is_ignore_request_error(error):
+                return self._handle_request_error(error)
+            else:
+                raise
---
+++ ifv/test_usage.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+
+import mock
+
+import ifv
+
+
+class TestBaseAPIItem(unittest.TestCase):
+
+    def test_contruction(self):
+        context = ifv.BaseAPIItem(name="test", value=1)
+        self.assertEqual(context["name"], "test")
+        self.assertEqual(context["value"], 1)
+
+    def test_merged_context(self):
+        parent_item = ifv.BaseAPIItem(name="parent", value=1)
+        child_item = ifv.BaseAPIItem(parent_item, name="child")
+        self.assertDictEqual(parent_item._merged_context, {
+            "name": "parent", "value": 1,
+        })
+        self.assertDictEqual(child_item._merged_context, {
+            "name": "child", "value": 1,
+        })
+
+    def test_parent_item(self):
+        parent_item = ifv.BaseAPIItem(name="parent", value=1)
+        child_item = ifv.BaseAPIItem(parent_item, name="child")
+        self.assertEqual(parent_item["name"], "parent")
+        self.assertEqual(parent_item["value"], 1)
+        self.assertEqual(child_item["name"], "child")
+        self.assertEqual(child_item["value"], 1)
+        self.assertEqual(len(child_item), 2)
+
+        with self.assertRaises(KeyError):
+            parent_item["nothing"]
+
+        with self.assertRaises(KeyError):
+            child_item["nothing"]
+
+        child_item["value"] = 2
+        self.assertEqual(child_item["value"], 2)
+        self.assertEqual(parent_item["value"], 1)
+
+    def test_copy(self):
+        parent_item = ifv.BaseAPIItem(name="parent", value=1)
+        child_item1 = ifv.BaseAPIItem(parent_item, name="child", items=[])
+        child_item2 = child_item1._copy()
+        self.assertIs(child_item1._parent_item, child_item2._parent_item)
+        self.assertIs(child_item1["items"], child_item2["items"])
+        self.assertEqual(child_item1["name"], child_item2["name"])
+
+    def test_subitem(self):
+        parent_item = ifv.BaseAPIItem(name="parent", value=1)
+        child_item = parent_item.child_item
+        self.assertIs(parent_item, child_item._parent_item)
+        self.assertDictEqual(
+            parent_item._merged_context, child_item._merged_context,
+        )
+
+        with mock.patch(
+            "ifv.BaseAPIItem._get_subitem_context",
+            side_effect=lambda name: {"name": name},
+        ):
+            child_item2 = parent_item.child_item2
+            self.assertIs(parent_item, child_item2._parent_item)
+            self.assertEqual(child_item2["name"], "child_item2")
+
+    def test_call(self):
+        parent_item = ifv.BaseAPIItem()
+        with mock.patch(
+            "ifv.BaseAPIItem._get_response_result",
+            side_effect=lambda **kwargs: kwargs,
+        ):
+            result = parent_item.item.get(value=1)
+            self.assertEqual(result, {"value": 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
